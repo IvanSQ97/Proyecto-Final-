01c15e3cdfdda2834f45c1f9206f87e4a3b71ff6
Ivan SQ <isanchezq002@alumno.uaemex.mx>
2023-12-03 23:21:00 -0600

    Modifico ProductoElectrónico

--- Proyecto-Final/ProductoElectronico.java
+++ Proyecto-Final/ProductoElectronico.java
@@ -1,18 +1,11 @@
-public class ProductoElectronico extends Producto {
+public class ProductoElectronico extends ProductoBase {
     private String marca;
 
     public ProductoElectronico(String nombre, double precio, String marca) {
         super(nombre, precio);
         this.marca = marca;
     }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+    
 
     @Override
     public void mostrarInformacion() {
@@ -20,4 +13,13 @@ public class ProductoElectronico extends Producto {
         System.out.println("Marca: " + marca);
     }
 
+
+    public String getMarca() {
+        return marca;
+    }
+
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
